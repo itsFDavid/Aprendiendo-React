3af5846861764cef7a0695a14ad351db9c88eb68
itsFDavid <fdavid04@icloud.com>
2024-07-23 15:41:24 -0600

    chore: add functions to send messages and update component

--- first-app/src/components/WriteMessage.jsx
+++ first-app/src/components/WriteMessage.jsx
@@ -1,14 +1,47 @@
 import '../App.css';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-export function WriteMessage() {
+export function WriteMessage({ socket, onNameChange }) {
+    const [name, setName] = useState(localStorage.getItem('name') || '');
+    const [message, setMessage] = useState('');
+
+    WriteMessage.propTypes = {
+        socket: PropTypes.object.isRequired,
+        onNameChange: PropTypes.func.isRequired,
+    };
+
+    const handleSendMessage = (e) => {
+        e.preventDefault();
+        if (message.trim() !== '') {
+            localStorage.setItem('name', name);
+            onNameChange(name);
+            socket.emit('chat message', message, name, socket.auth.serverOffset);
+            setMessage(''); // Clear the message input after sending
+        } else {
+            alert('Please write a message');
+        }
+    };
 
     return (
         <div className='write-message'>
-            <form className='send-message'>
-                <input type='text' placeholder='Name' />
-                <input type='text' placeholder='Message' />
-                <button className='button-send'>Send Message</button>
+            <form className='send-message' onSubmit={handleSendMessage}>
+                <input
+                    type='text'
+                    id='name'
+                    value={name}
+                    placeholder='Name'
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type='text'
+                    id='message'
+                    value={message}
+                    placeholder='Message'
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button className='button-send' type='submit'>Send Message</button>
             </form>
         </div>
-    )
+    );
 }
